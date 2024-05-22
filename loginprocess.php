183e65212beda2908bdbb24cce543ea9f140fc61
hy02-bug <yadiy021102@gmail.com>
2024-05-22 19:48:44 +0800

    Update loginprocess.php

--- loginprocess.php
+++ loginprocess.php
@@ -9,6 +9,7 @@ $username=@$_POST["username"];
 $password=@$_POST["pass"];
 $sql="SELECT * FROM users WHERE username='$username'";
 $result=mysqli_query($con,$sql);
+
 if(mysqli_num_rows($result) == 0){
 echo "Username does not exist";
 }
@@ -17,11 +18,12 @@ else
     $row=mysqli_fetch_array($result,MYSQLI_BOTH);
     if($row["password"] ==$password)
     {
-       // session_start();
-        //$_SESSION["username"]= $username;
+        session_start();
+        $_SESSION["username"]= $username;
+        $_SESSION["user_id"] = $row["user_id"];
         //go to userindex.php page
-        //header("Location:userindex.php");
-        echo "Success";
+        header("Location:index_usermain.php");
+       // echo "Success";
     }
     else
     echo "Password wrong";
